abfdf57df9908c9a633df4d6f311bfbf4a8557f4
Ethan McCue <emccue@live.com>
2016-01-27 12:04:04 -0500

    added prototypes for the create functions

--- wallaby.py
+++ wallaby.py
@@ -4,11 +4,12 @@ module for wrapping all necessary wallaby functions
 The fact I have to write this is ridiculous
 '''
 
-from ctypes import CDLL, c_int
+from ctypes import CDLL, c_int, c_short
 from decorators import accepts, returns, cast_to
 
-
+#CDLL assumes that all functions return a c_int
 _wallaby = CDLL("libwallaby.so")
+
 
 '''
 BEGIN MOTOR FUNCTIONS
@@ -300,15 +301,21 @@ int accel_calibrate();
 
 '''
 
+_wallaby.accel_x.restype = c_short
+
 @returns(int)
 def accel_x():
     '''gets the x value of the accelerometer'''
     return int(_wallaby.accel_x())
 
+_wallaby.accel_y.restype = c_short
+
 @returns(int)
 def accel_y():
     '''gets the y value of the accelerometer'''
     return int(_wallaby.accel_y())
+
+_wallaby.accel_z.restype = c_short
 
 @returns(int)
 def accel_z():
@@ -383,3 +390,273 @@ def get_digital_pullup(port):
 def set_digital_pullup(port, pullup):
     '''sets digital pullup'''
     _wallaby.set_digital_pullup(port, pullup)
+
+
+r'''
+BEGIN CREATE FUNCTIONS
+
+/*!
+ * Waits to establish a connection to the create.
+ * \blocksuntil A connection to a create is established
+ * \return 0 on success
+ * \see create_disconnect
+ * \ingroup create
+ */
+ int create_connect();
+
+/*!
+ * Attempts to establish a connection to the create.
+ * \return 1 if connection succeeded, 0 if connection failed
+ * \see create_disconnect
+ * \ingroup create
+ */
+ int create_connect_once();
+
+/*!
+ * Cleans up connection to the create.
+ * \see create_connect
+ * \ingroup create
+ */
+ void create_disconnect();
+
+
+ void create_start();
+
+
+ void create_passive();
+
+
+ void create_safe();
+
+
+ void create_full();
+
+
+ void create_spot();
+
+
+ void create_cover();
+
+
+ void create_demo(int d);
+
+
+ void create_cover_dock();
+
+
+ int get_create_mode();
+
+
+ int get_create_lbump();
+
+
+ int get_create_rbump();
+
+
+ int get_create_lwdrop();
+
+
+ int get_create_cwdrop();
+
+
+ int get_create_rwdrop();
+
+
+ int get_create_wall();
+
+
+ int get_create_lcliff();
+
+
+ int get_create_lfcliff();
+
+
+ int get_create_rfcliff();
+
+
+ int get_create_rcliff();
+
+
+ int get_create_llightbump();
+
+
+ int get_create_lflightbump();
+
+
+ int get_create_lclightbump();
+
+
+ int get_create_rclightbump();
+
+
+ int get_create_rflightbump();
+
+
+ int get_create_rlightbump();
+
+
+ int get_create_llightbump_amt();
+
+
+ int get_create_rlightbump_amt();
+
+
+ int get_create_lflightbump_amt();
+
+
+ int get_create_lclightbump_amt();
+
+
+ int get_create_rclightbump_amt();
+
+
+ int get_create_rflightbump_amt();
+
+
+ int get_create_vwall();
+
+
+ int get_create_overcurrents();
+
+
+ int get_create_infrared();
+
+
+ int get_create_advance_button();
+
+
+ int get_create_play_button();
+
+
+ int get_create_normalized_angle();
+
+
+ void set_create_normalized_angle(int angle);
+
+
+ int get_create_total_angle();
+
+
+ void set_create_total_angle(int angle);
+
+
+ int get_create_distance();
+
+
+ void set_create_distance(int dist);
+
+
+ int get_create_battery_charging_state();
+
+
+ int get_create_battery_voltage();
+
+
+ int get_create_battery_current();
+
+
+ int get_create_battery_temp();
+
+
+ int get_create_battery_charge();
+
+
+ int get_create_battery_capacity();
+
+
+ int get_create_wall_amt();
+
+
+ int get_create_lcliff_amt();
+
+
+ int get_create_lfcliff_amt();
+
+
+ int get_create_rfcliff_amt();
+
+
+ int get_create_rcliff_amt();
+
+
+ int get_create_bay_DI();
+
+
+ int get_create_bay_AI();
+
+
+ int get_create_song_number();
+
+
+ int get_create_song_playing();
+
+
+ int get_create_number_of_stream_packets();
+
+
+ int get_create_requested_velocity();
+
+
+ int get_create_requested_radius();
+
+
+ int get_create_requested_right_velocity();
+
+
+ int get_create_requested_left_velocity();
+
+
+ void create_stop();
+
+
+ void create_drive(int speed, int radius);
+
+
+ void create_drive_straight(int speed);
+
+
+ void create_spin_CW(int speed);
+
+
+ void create_spin_CCW(int speed);
+
+
+ void create_drive_direct(int l_speed, int r_speed);
+
+
+ void create_spin_block(int speed, int angle);
+
+
+ void create_advance_led(int on);
+
+
+ void create_play_led(int on) ;
+
+
+ void create_power_led(int color, int brightness);
+
+
+ void create_digital_output(int bits);
+
+
+ void create_pwm_low_side_drivers(int pwm2, int pwm1, int pwm0);
+
+
+ void create_low_side_drivers(int pwm2, int pwm1, int pwm0);
+
+
+ void create_load_song(int num);
+
+
+ void create_play_song(int num);
+
+
+ int create_read_block(char *data, int count);
+
+
+ void create_write_byte(char byte);
+
+
+ void create_clear_serial_buffer();
+
+'''
