c78ad9d4555ee4a38b120275f73be0ca99355028
Ethan McCue <emccue@live.com>
2016-01-30 18:23:26 -0500

    adding license to movement.py

--- movement.py
+++ movement.py
@@ -1,4 +1,20 @@
-'''movement functions for a two wheeled robot'''
+"""
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    movement functions for a two wheeled robot
+"""
+
 from __future__ import division
 from wallaby import motor, off
 from decorators import accepts
